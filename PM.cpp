eb4d5bb106c7b7e5a49e1b07f0fd4811836aec2e
Hsinhao <b03202022@ntu.edu.tw>
2019-05-20 23:17:37 +0800

    draft version of poissonsolver using FFTW under isolated BC

--- PM.cpp
+++ PM.cpp
@@ -76,26 +76,73 @@ void PoissonSolver( double rho[N][N][N], double phi[N][N][N] ){
        for(int i=0;i<N;i++)
           for(int j=0;j<N;j++)
              for(int k=0;k<N/2+1;k++){
-                out[i][j][k][0] = out[i][j][k][0]/Ksquare[i][j][k]*(-1.0/(N*N*N));
-                out[i][j][k][1] = out[i][j][k][1]/Ksquare[i][j][k]*(-1.0/(N*N*N));
+                out[i][j][k][0] = out[i][j][k][0]/Ksquare[i][j][k]*4.0*M_PI*G*(-1.0/(N*N*N));
+                out[i][j][k][1] = out[i][j][k][1]/Ksquare[i][j][k]*4.0*M_PI*G*(-1.0/(N*N*N));
              }
        fftw_execute(q);
 
-       fftw_destroy_plan(q);
+       fftw_destroy_plan(p);
        fftw_destroy_plan(q);
     }
     else if(BC==2){
        if(Scheme_PS==1){
-          ;
+          double rho_zeropadding[2*N][2*N][2*N];
+          double Greens_R[2*N][2*N][2*N];
+          double phi_zeropadding[2*N][2*N][2*N];
+
+          for(int i=0;i<N;i++)
+             for(int j=0;j<N;j++)
+                for(int k=0;k<N;k++){
+                   rho_zeropadding[i][j][k] = rho[i][j][k];
+                   if( i==0 && j==0 && k==0) Greens_R[i][j][k] = 0.0;
+                   else Greens_R[i][j][k] = -1.0/sqrt(i*i+j*j+k*k);
+                }
+
+          for(int i=N;i<2*N;i++)
+             for(int j=N;j<2*N;j++)
+                for(int k=N;k<2*N;k++){
+                   rho_zeropadding[i][j][k] = 0.0;
+                   Greens_R[i][j][k] = -1.0/sqrt((2*N-i)*(2*N-i)+(2*N-j)*(2*N-j)+(2*N-k)*(2*N-k));
+                }
+
+       
+          fftw_complex rho_zeropadding_K[2*N][2*N][N+1];
+          fftw_complex Greens_R_K[2*N][2*N][N+1];
+          fftw_complex out[2*N][2*N][N+1];
+          fftw_plan p_rho, p_greens, q;
+          p_rho = fftw_plan_dft_r2c_3d( 2*N, 2*N, 2*N, &rho_zeropadding[0][0][0], &rho_zeropadding_K[0][0][0], FFTW_ESTIMATE );
+          p_greens = fftw_plan_dft_r2c_3d( 2*N, 2*N, 2*N, &Greens_R[0][0][0], &Greens_R_K[0][0][0], FFTW_ESTIMATE );
+          q = fftw_plan_dft_c2r_3d( N, N, N, &out[0][0][0], &phi_zeropadding[0][0][0], FFTW_ESTIMATE );
+          
+          fftw_execute(p_rho);
+          fftw_execute(p_greens);
+          for(int i=0;i<2*N;i++)
+             for(int j=0;j<2*N;j++)
+                for(int k=0;k<N+1;k++){
+                   out[i][j][k][0] = ( rho_zeropadding_K[i][j][k][0]*Greens_R_K[i][j][k][0]-rho_zeropadding_K[i][j][k][1]*Greens_R_K[i][j][k][1])*(G*dx*dx/(2*N*2*N*2*N));
+                   out[i][j][k][1] = ( rho_zeropadding_K[i][j][k][0]*Greens_R_K[i][j][k][1]+rho_zeropadding_K[i][j][k][1]*Greens_R_K[i][j][k][0])*(G*dx*dx/(2*N*2*N*2*N));
+                }
+          fftw_execute(q);
+
+          fftw_destroy_plan(p_rho);
+          fftw_destroy_plan(p_greens);
+          fftw_destroy_plan(q);
+
+          for(int i=0;i<N;i++)
+             for(int j=0;j<N;j++)
+                for(int k=0;k<N;k++)
+                   phi[i][j][k] = phi_zeropadding[i][j][k];
+
+
        }
        else if(Scheme_PS==2){
           ;
        }
        else
-          printf("ERROR: Unrecognized Scheme_PS!\n");
+          printf("ERROR: Unsupported Scheme_PS!\n");
     }
     else
-       printf("ERROR: Unrecognized Boundary Condition!\n");
+       printf("ERROR: Unsuppoted Boundary Condition!\n");
 
 
     return;
